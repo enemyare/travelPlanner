c6fa6226ead82a965bfe6c84509d044a6ded2260
enemyare <mishasultanov4@gmail.com>
2025-03-24 11:33:23 +0500

    Добавил таблицу

---
+++ frontend/src/components/Table.component.tsx
@@ -0,0 +1,121 @@
+import { Table, Button } from '@gravity-ui/uikit';
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+import './table.css';
+
+const landmarks = [
+  {
+    id: 1,
+    name: 'Эйфелева башня',
+    description: 'Знаменитая башня в Париже.',
+    rating: 5,
+    status: 'В планах',
+    location: 'Париж, Франция',
+    coordinates: "48.8584, 2.2945",
+    mapLink: 'https://maps.google.com/?q=48.8584,2.2945',
+  },
+  {
+    id: 2,
+    name: 'Колизей',
+    description: 'Амфитеатр в Риме, символ древней истории.',
+    rating: 4,
+    status: 'Осмотрена',
+    location: 'Рим, Италия',
+    image: 'https://sun9-9.userapi.com/impg/0e9ec2gq78lmC4sUqU9m2M5wL8i0GVS-pGN3LA/83zcgL815AA.jpg?size=1620x2160&quality=95&sign=9c5b9599f1b8f584112db9e6081e6d5c&type=album',
+    coordinates: "41.8902, 12.4922",
+    mapLink: 'https://maps.google.com/?q=41.8902,12.4922',
+  },
+  {
+    id: 3,
+    name: 'Статуя Свободы',
+    description: 'Символ свободы и демократии в Нью-Йорке. dsdadasdasdasdsadasdasdasdasdadasdasdasdasdasdasdassdasdasdssad',
+    rating: 3,
+    status: 'В планах',
+    location: 'Нью-Йорк, США',
+    coordinates: "40.6892, -74.0445",
+    mapLink: 'https://maps.google.com/?q=40.6892,-74.0445',
+  },
+];
+
+
+const columns = [
+  {
+    id: 'id',
+    name: 'ID',
+  },
+  {
+    id: 'name',
+    name: 'Название',
+  },
+  {
+    id: 'description',
+    name: 'Описание',
+    className: 'description-cell'
+  },
+  {
+    id: 'createdAt',
+    name: 'Добавлено',
+  },
+  {
+    id: 'rating',
+    name: 'Рейтинг',
+  },
+  {
+    id: 'image',
+    name: 'Фото',
+    template: (item: any) => (
+      item.image ?
+      <img
+        src={item.image}
+        style={{ width: '100px', height: 'auto', borderRadius: '4px' }}
+      />:
+        <span>Отсутствует</span>
+    ),
+  },
+  {
+    id: 'location',
+    name: 'Местоположение',
+  },
+  {
+    id: 'coordinates',
+    name: 'Координаты',
+  },
+  {
+    id: 'mapLink',
+    name: 'Ссылка на карту',
+    template: (item: any) => (
+      <a href={item.mapLink} target="_blank" rel="noopener noreferrer">
+        Открыть карту
+      </a>
+    ),
+  },
+  {
+    id: 'status',
+    name: 'Статус',
+  },
+  {
+    id: 'actions',
+    name: 'Действия',
+    template: (item: any) => (
+      <Button
+        view="outlined"
+        onClick={() => {item}}
+      >
+        Выбрать
+      </Button>
+    ),
+  },
+];
+
+
+const LandmarksTable = () => {
+  return (
+    <Table
+      data={landmarks}
+      columns={columns}
+      emptyMessage="Достопримечательности не найдены"
+    />
+  );
+};
+
+export default LandmarksTable;
